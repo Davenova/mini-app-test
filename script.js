9ca37a8d68b50400829e4707b35f61e22471cf98
Shubz100 <115923262+Shubz100@users.noreply.github.com>
2024-10-04 12:31:30 +0530

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let clicked = false;
 button.addEventListener('click', () => {
   if (!clicked) {
     button.textContent = 'Claim';
-    window.open('https://www.youtube.com/', '_blank'); // Replace with your YouTube link
+    window.open('https://youtu.be/dQw4w9WgXcQ?si=rUeV7tTXonGic1D_', '_blank'); // Replace with your YouTube link
     clicked = true;
   } else if (clicked) {
     button.classList.remove('bg-blue-500', 'hover:bg-blue-700');

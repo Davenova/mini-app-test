bfda6cfc8c4f078ca7a0e08adc344e1596d79be9
Shubz100 <115923262+Shubz100@users.noreply.github.com>
2024-10-04 13:38:29 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let chatId;
+let claimed = localStorage.getItem('claimed_button'); // Check if claimed_button is set in localStorage
 
 // Retrieve chat ID from Telegram Bot API
 fetch(`https://api.telegram.org/bot7685147619:AAGivfwk7FFDwbldf47l0-ktlK1TEpMoT78/getUpdates`, {
@@ -14,13 +15,23 @@ fetch(`https://api.telegram.org/bot7685147619:AAGivfwk7FFDwbldf47l0-ktlK1TEpMoT7
 });
 
 const button = document.getElementById('myButton');
-let clicked = false;
+
+if (claimed === 'true') {
+  // If claimed_button is set to true in localStorage, update the button state
+  button.textContent = 'Claimed';
+  button.classList.remove('bg-blue-500', 'hover:bg-blue-700');
+  button.classList.add('bg-white', 'text-green-500');
+} else {
+  button.textContent = 'Check';
+}
 
 button.addEventListener('click', () => {
-  if (!clicked) {
+  if (!claimed) {
     button.textContent = 'Claim';
     window.open('https://youtu.be/dQw4w9WgXcQ?si=rUeV7tTXonGic1D_', '_blank'); // Replace with your YouTube link
-    clicked = true;
+
+    // Set claimed_button to true in localStorage
+    localStorage.setItem('claimed_button', 'true');
 
     // Send request to Telegram Bot API to update user's status
     fetch(`https://api.telegram.org/bot7685147619:AAGivfwk7FFDwbldf47l0-ktlK1TEpMoT78/setChatData`, {
@@ -34,10 +45,11 @@ button.addEventListener('click', () => {
         value: 'true'
       })
     });
-  } else if (clicked) {
-    button.classList.remove('bg-blue-500', 'hover:bg-blue-700');
-    button.classList.add('bg-white', 'text-green-500');
-    button.textContent = 'Claimed';
-    clicked = false; // Reset the clicked variable to false
+  } else {
+    button.classList.remove('bg-white', 'text-green-500');
+    button.classList.add('bg-blue-500', 'hover:bg-blue-700');
+    button.textContent = 'Check';
+    localStorage.removeItem('claimed_button'); // Remove claimed_button from localStorage
   }
+  claimed = localStorage.getItem('claimed_button'); // Update claimed variable
 });

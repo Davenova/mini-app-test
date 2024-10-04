f57f1d1cea3d9a13926a47e3713da5051ba92c2c
Shubz100 <115923262+Shubz100@users.noreply.github.com>
2024-10-04 11:07:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('myButton').addEventListener('click', function() {
+  this.textContent = 'Claim';
+  this.innerHTML = '<a href="https://www.youtube.com/" target="_blank">Claim</a>';
+});

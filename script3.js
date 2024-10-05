89401d56ef9e8f41cd76e7faf6566364d042c17e
Shubz100 <115923262+Shubz100@users.noreply.github.com>
2024-10-05 23:12:15 +0530

    Delete script3.js

--- script3.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    // Hide the loading animation and show the home page after 10 seconds
-    setTimeout(function() {
-        $('#loading-container').hide();
-        $('#home-container').show();
-    }, 10000);
-
-    // Your existing JavaScript code here...
-});

ae6f20c34a024f04e3a04e5ecaa3591276c100e1
Shubz100 <115923262+Shubz100@users.noreply.github.com>
2024-10-06 00:32:29 +0530

    script2.js

---
+++ script2.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+  var counter = 0;
+
+  // Hide the loading animation and show the home page after 10 seconds
+  setTimeout(function() {
+    $('#loading-container').hide();
+    $('#home-container').show();
+  }, 5000);
+
+  // Start the changing images
+  setInterval(function() {
+    if(counter == 9) { 
+      counter = 0; 
+    }
+
+    changeImage(counter);
+    counter++;
+  }, 3000);
+
+  // Set the percentage off
+  loading();
+
+  // Your existing JavaScript code here...
+});
+
+function changeImage(counter) {
+  var images = [
+    '<i class="fa fa-fighter-jet"></i>',
+    '<i class="fa fa-gamepad"></i>',
+    '<i class="fa fa-headphones"></i>',
+    '<i class="fa fa-cubes"></i>',
+    '<i class="fa fa-paw"></i>',
+    '<i class="fa fa-rocket"></i>',
+    '<i class="fa fa-ticket"></i>',
+    '<i class="fa fa-pie-chart"></i>',
+  ];
+
+  $(".loader .image").html(""+ images[counter] +"");
+}
+
+function loading(){
+  var num = 0;
+
+  for(i=0; i<=100; i++) {
+    setTimeout(function() { 
+      $('.loader span').html(num+'%');
+
+      if(num == 100) {
+        loading();
+      }
+      num++;
+    },i*50);
+  };
+}
